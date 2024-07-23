a2d3165ad3be55b937adf0a428e4a518b3796978
Aditya Patel <adityadipakpatel@gmail.com>
2024-07-23 03:58:34 -0600

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,5 +1,5 @@
-y1 = input('Pass1: ')
-y2 = input('Pass2: ')
+y1 = int(input('Pass1: '))
+y2 = int(input('Pass2: '))
 x1, x2 = 1, 2
 
 def decrypt_func(text):
@@ -7,7 +7,7 @@ def decrypt_func(text):
     return ''.join([chr(int(text[i:i+3])) for i in range(0, len(text), 3)])
 
 #check if y1, y2 start with '0'
-is_zero = (input('Was there a zero at the start? (y/n): ')).lower
+is_zero = (input('Was there a zero at the start? (y/n): ')).lower()
 
 is_zero = True if is_zero == 'y' else False
 
@@ -20,5 +20,5 @@ plain_text = f"0{plain_text}" if is_zero else plain_text
 
 plain_text = decrypt_func(plain_text)
 print(f'Slope (m): {m}')
-print(f'Orignal Text: {plain_text}')
+print(f'Original Text: {plain_text}')
 
